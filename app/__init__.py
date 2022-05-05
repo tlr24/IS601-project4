05acaba73f166a8412fe6406184666f7289939b2
tlr24 <tlr24@njit.edu>
2022-05-05 11:14:09 -0400

    Added creation of db & test

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from app.cli import create_database
-from app.db import db
+from app.db import db, database
 from app.db.models import User
 from app.simple_pages import simple_pages
 
@@ -24,13 +24,11 @@ def create_app():
     app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
     bootstrap = Bootstrap5(app)
     app.register_blueprint(simple_pages)
-    app.register_error_handler(404, page_not_found)
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+    app.register_blueprint(database)
     # add command function to cli commands
     app.cli.add_command(create_database)
+    app.register_error_handler(404, page_not_found)
+    db.init_app(app)
 
 
     return app
