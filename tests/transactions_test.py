5120d885bec1d358b6aa52a8342da805ef1194a8
tlr24 <tlr24@njit.edu>
2022-05-09 12:36:31 -0400

    Added user balance to dashboard & added test for calculating balance

--- tests/transactions_test.py
+++ tests/transactions_test.py
@@ -1,4 +1,4 @@
-"""Tests the songs functionality"""
+"""Tests the transaction functionality"""
 import pytest
 import csv
 import os
@@ -17,7 +17,8 @@ def write_test_csv():
     header = ['AMOUNT', 'TYPE']
     data = [
         ['100', "CREDIT"],
-        ['200', "DEBIT"],
+        ['-200', "DEBIT"],
+        ['300', "CREDIT"],
     ]
 
     with open('transaction_test.csv', 'w', encoding='UTF8', newline='') as f:
@@ -40,7 +41,7 @@ def test_updating_transactions(application, add_user_to_db):
     """Test updating transaction"""
     with application.app_context():
         user = User.query.filter_by(email="a@gmail.com").first()
-        # prepare songs to edit
+        # prepare transactions to edit
         user.transactions = [Transaction("100", "CREDIT")]
         db.session.commit()
         # changing the amount of the transaction
@@ -51,7 +52,7 @@ def test_updating_transactions(application, add_user_to_db):
         updated_transaction = Transaction.query.filter_by(amount='10000000').first()
         assert updated_transaction.amount == "10000000"
 
-def test_deleting_song(application, add_user_to_db):
+def test_deleting_transaction(application, add_user_to_db):
     """Test deleting the transaction"""
     user = User.query.filter_by(email="a@gmail.com").first()
     # prepare transaction to insert
@@ -80,7 +81,20 @@ def test_upload_csv(client, add_user, write_test_csv):
 
     # test that the csv file was processed and the transactions were inserted into the database
     user = User.query.filter_by(email="a@a.com").first()
-    assert len(user.transactions) == 2
-    assert db.session.query(Transaction).count() == 2
+    assert len(user.transactions) == 3
+    assert db.session.query(Transaction).count() == 3
     assert Transaction.query.filter_by(type="CREDIT").first() is not None
     assert Transaction.query.filter_by(type="DEBIT").first() is not None
+
+def test_get_user_balance(client, add_user, write_test_csv):
+    """Test correctly calculating and getting the user's balance on the dashboard"""
+    # login to upload the csv
+    client.post("/login", data={'email': 'a@a.com', 'password': '123La!'})
+    # upload the test csv
+    file = open("transaction_test.csv", 'rb')
+    response = client.post('/transactions/upload', data={'file': file})
+    assert "/transactions" in response.headers["Location"]
+
+    # test if the balance is calculated correctly on the dashboard
+    response = client.get('/dashboard')
+    assert b"Balance: 200.0" in response.data
